78d1d05723b9c89c6d612b00a8d4fd5d3ce8118f
Steven Moreland <smoreland@google.com>
2017-03-16 02:04:44 -0700

    TokenManager: use secure tokens
    Token manager tokens need to be larger then 64 bits so that we have
    sufficient security. Allowing them to be variably sized gives us
    sufficient flexibility to change the underlying implementation without
    having to change the interface.
    
    Bug: 33842662
    Test: hidl_test
    Test: (sanity) YouTube + Google Play Movies works
    Test: camera video recording works
    Test: lshal
    
    Change-Id: I6a39e68b6b660cf00bf5363d9c66a8fa1ff2c6a5

--- TokenManager.cpp
+++ TokenManager.cpp
@@ -4,6 +4,8 @@
 
 #include <android-base/logging.h>
 #include <functional>
+#include <openssl/hmac.h>
+#include <openssl/rand.h>
 
 namespace android {
 namespace hidl {
@@ -11,20 +13,58 @@ namespace token {
 namespace V1_0 {
 namespace implementation {
 
-// Methods from ::android::hidl::token::V1_0::ITokenManager follow.
-Return<uint64_t> TokenManager::createToken(const sp<IBase>& store) {
-    uint64_t token;
-
-    do {
-        token = generateToken();
-    } while (mMap.find(token) != mMap.end()); // unlikely to ever happen
-
-    mMap[token] = store;
-    return token;
+TokenManager::TokenManager() {
+    RAND_bytes(mKey.data(), mKey.size());
 }
 
-Return<bool> TokenManager::unregister(uint64_t token) {
-    auto it = mMap.find(token);
+// Methods from ::android::hidl::token::V1_0::ITokenManager follow.
+Return<void> TokenManager::createToken(const sp<IBase>& store, createToken_cb hidl_cb) {
+    TokenInterface interface = generateToken(store);
+
+    if (interface.interface == nullptr) {
+        hidl_cb({});
+        return Void();
+    }
+
+    uint64_t id = getTokenId(interface.token);
+
+    if (id == TOKEN_ID_NONE) {
+        hidl_cb({});
+        return Void();
+    }
+
+    mMap[id] = interface;
+
+    hidl_cb(interface.token);
+    return Void();
+}
+
+std::unordered_map<uint64_t,  TokenManager::TokenInterface>::const_iterator
+        TokenManager::lookupToken(const hidl_vec<uint8_t> &token) {
+    uint64_t tokenId = getTokenId(token);
+
+    if (tokenId == TOKEN_ID_NONE) {
+        return mMap.end();
+    }
+
+    auto it = mMap.find(tokenId);
+
+    if (it == mMap.end()) {
+        return mMap.end();
+    }
+
+    const TokenInterface &interface = it->second;
+
+    if (!constantTimeCompare(token, interface.token)) {
+        ALOGE("Fetch of token with invalid hash.");
+        return mMap.end();
+    }
+
+    return it;
+}
+
+Return<bool> TokenManager::unregister(const hidl_vec<uint8_t> &token) {
+    auto it = lookupToken(token);
 
     if (it == mMap.end()) {
         return false;
@@ -34,21 +74,85 @@ Return<bool> TokenManager::unregister(uint64_t token) {
     return true;
 }
 
-Return<sp<IBase>> TokenManager::get(uint64_t token) {
-    const auto it = mMap.find(token);
+Return<sp<IBase>> TokenManager::get(const hidl_vec<uint8_t> &token) {
+    auto it = lookupToken(token);
 
     if (it == mMap.end()) {
         return nullptr;
     }
 
-    return it->second;
+    return it->second.interface;
 }
 
-uint64_t TokenManager::generateToken() {
-    // TODO(b/33842662): pending security review
-    // TODO(b/33842662): use cryptographic hash function, this is a placeholder
-    return std::hash<uint64_t>{}(mSalt + mTokenIndex++);
+
+TokenManager::TokenInterface TokenManager::generateToken(const sp<IBase> &interface) {
+    uint64_t id = ++mTokenIndex;
+
+    std::array<uint8_t, EVP_MAX_MD_SIZE> hmac;
+    uint32_t hmacSize;
+
+    uint8_t *hmacOut = HMAC(EVP_sha256(),
+                            mKey.data(), mKey.size(),
+                            (uint8_t*) &id, ID_SIZE,
+                            hmac.data(), &hmacSize);
+
+    if (hmacOut == nullptr ||
+            hmacOut != hmac.data()) {
+        ALOGE("Generating token failed, got %p.", hmacOut);
+        return { nullptr, {} };
+    }
+
+    // only care about the first HMAC_SIZE bytes of the HMAC
+    const hidl_vec<uint8_t> &token = TokenManager::getToken(id, hmac.data(), hmacSize);
+
+    return { interface, token };
 }
+
+// static
+__attribute__((optnone))
+bool TokenManager::constantTimeCompare(const hidl_vec<uint8_t> &t1, const hidl_vec<uint8_t> &t2) {
+    if (t1.size() != t2.size()) {
+        return false;
+    }
+
+    uint8_t x = 0;
+    for (size_t i = 0; i < t1.size(); i++) {
+        x |= t1[i] ^ t2[i];
+    }
+
+    return x == 0;
+}
+
+// static
+uint64_t TokenManager::getTokenId(const hidl_vec<uint8_t> &token) {
+    if (token.size() < ID_SIZE) {
+        return TOKEN_ID_NONE;
+    }
+
+    uint64_t id = 0;
+    for (size_t i = 0; i < ID_SIZE; i++) {
+        id |= token[i] << i;
+    }
+
+    return id;
+}
+
+// static
+hidl_vec<uint8_t> TokenManager::getToken(const uint64_t id, const uint8_t *hmac, uint64_t hmacSize) {
+    hidl_vec<uint8_t> token;
+    token.resize(ID_SIZE + hmacSize);
+
+    for (size_t i = 0; i < ID_SIZE; i++) {
+        token[i] = (id >> i) & 0xFF;
+    }
+
+    for (size_t i = 0; i < hmacSize; i++) {
+        token[i + ID_SIZE] = hmac[i];
+    }
+
+    return token;
+}
+
 
 }  // namespace implementation
 }  // namespace V1_0
